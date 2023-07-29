b0c0f3c6341b7ea4eadf012d6d16a83963096779
Jean Bosco Mugiraneza <mjeanbosco19@gmail.com>
2023-07-29 12:05:43 +0200

    TypeScript with JavaScript

---
+++ dist/modules/defaultExport.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Format = void 0;
+class Storer {
+}
+exports.default = Storer;
+var Format;
+(function (Format) {
+    Format[Format["Raw"] = 0] = "Raw";
+    Format[Format["Compressed"] = 1] = "Compressed";
+})(Format || (exports.Format = Format = {}));
+class Compressor {
+}
+class Encryptor {
+}
+//# sourceMappingURL=defaultExport.js.map
---
+++ src/javascript/tax.js
@@ -0,0 +1,8 @@
+/**
+ * Calculates the tax for a given income
+ * @param {number} income - The income to calculate the tax for
+ * @returns {number}
+ */
+export function calculateTax(income){
+    return income * 0.3;
+}
